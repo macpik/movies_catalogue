52c7c1681c85fc4e92c588b5b9f969f3ca5ab560
Maciek Pikula <maciej.pikula@icloud.com>
2023-06-26 16:56:49 +0100

    add structure

--- tmdb_client.py
+++ tmdb_client.py
@@ -1,8 +1,9 @@
-import requests
+import requests, random
+
+api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YzEwZDg3YmM3MzYyMjUzZDY5NjE2ZGE3NzEwN2YzYSIsInN1YiI6IjY0OTNmNTNhY2ZlNDhmMDExZDQ0YjUyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bWu3BxPkbKH4xrAc4pq4IiNzqET4RAh-Gq93OnL0WsI"
 
 def get_popular_movies():
-    endpoint = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
-    api_token = "7c10d87bc7362253d69616da77107f3a"
+    endpoint = "https://api.themoviedb.org/3/movie/popular"
     headers = {
         "Authorization": f"Bearer {api_token}"
     }
@@ -12,3 +13,32 @@ def get_popular_movies():
 def get_poster_url(poster_api_path, size = "w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
+
+def get_movies(how_many, list_type):
+    data = get_movies_list()
+    return data["results"][:how_many][list_type]
+
+def get_single_movie(movie_id): 
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
+
+def get_movies_list(list_type): 
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
